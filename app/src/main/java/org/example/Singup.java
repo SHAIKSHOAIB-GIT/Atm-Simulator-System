eeec85c1e7d6f18fc005d8b33e7f07440634e7a7
shaikshoaib <shaikshoaib820@gmail.com>
2025-01-08 19:31:25 +0530

    add new file

---
+++ app/src/main/java/org/example/Singup.java
@@ -0,0 +1,14 @@
+// package org.example;
+// import javax.swing.JFrame;
+
+// // public class Singup extends JFrame {
+// //     // singUp(){
+// //     //     setSize(850,800);
+// //     //     setVisible(true);
+
+// //     }
+//     public static void main(String[] args) {
+//         // new Singup();
+        
+//     }
+// }
